f26803edca2c83f61153bb3c314f4f5321c41a18
mm0804mm <mm0804mm@naver.com>
2021-04-02 00:48:17 +0900

    .
    .

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ client = commands.Bot(command_prefix = '-')
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online)
-    await client.change_presence(activity=discord.Game(name="제3의 상현"))
+    await client.change_presence(activity=discord.Game(name="소울워커"))
     print("봇 이름:",client.user.name,"봇 아이디",client.user.id,"봇 버전:",discord.__version__)
 
 client.run(os.environ['token'])
